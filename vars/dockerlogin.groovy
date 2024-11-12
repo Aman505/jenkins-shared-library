a1b713cf656936c4b41ca3645075909532e783ce
Aman505 <57318113+Aman505@users.noreply.github.com>
2024-11-12 22:22:30 +0530

    Update dockerlogin.groovy

--- vars/dockerlogin.groovy
+++ vars/dockerlogin.groovy
@@ -1,1 +1,10 @@
-
+def call(String credID, String imagename, String imagetag){
+                  withCredentials([usernamePassword(
+                    credentialsId:"${credID}",
+                    usernameVariable:"dockerHubUser", 
+                    passwordVariable:"dockerHubPass")]){
+                sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
+                sh "docker image tag ${imagename}:${imagetag} ${env.dockerHubUser}/${imagename}:${imagetag}"
+                sh "docker push ${env.dockerHubUser}/${imagename}:${imagetag}"
+                }
+}
